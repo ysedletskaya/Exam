e5d51b93e466e0f1e98fd68dfe093a0f03160199
Mykola Sedletskyi <mykolas@nice.com>
2016-03-24 13:45:53 +0200

    Proejct for the examination

---
+++ ExamProject/ILinkedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProject
+{
+    public interface ILinkedList
+    {
+        void AddFirst(int value);
+        void AddLast(int value);
+        void Remove(int value);
+        void RemoveFirst();
+        void RemoveLast();
+        bool Find(int value);
+        int Lenght();
+    }
+}
